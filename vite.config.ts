b22d51242078f4bb5a11077111757af7d661e0a8
yusasa16 <yusasa16@gmail.com>
2022-10-09 15:34:18 +0900

    マルチページ化

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const root = resolve(__dirname, 'htdocs');
+const outDir = resolve(__dirname, 'dist');
+const publicDir = resolve(__dirname, 'public');
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	root: 'htdocs',
+	root,
+	publicDir,
+
   build: {
-    outDir: '../dist',
+    outDir,
+		rollupOptions: {
+			input: {
+				index: resolve(root, 'index.html'),
+				about: resolve(root, 'about', 'index.html')
+			}
+		}
   },
-	publicDir: "../public",
 
 	server: {
 		host: true,
